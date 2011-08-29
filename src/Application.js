55086928f05a49f59c8ba9b159c2fa918b73f6d6
Bartlomiej Niemtur <nbartlomiej@gmail.com>
2011-08-29 19:58:53 +0200

    application stub

---
+++ src/Application.js
@@ -0,0 +1,29 @@
+parser = new Parser();
+
+wordScanner = new Scanner(/\w/, new Counter());
+wordNotifier = new WordNotifier(wordScanner);
+
+parser.scanners.push( wordScanner);
+parser.notifiers.push( new WordNotifier(wordScanner) );
+
+var onLoad = function(){
+  var licenseTextarea = document.getElementById('license');
+  licenseTextarea.onkeyup = function(){
+    parser.parse(licenseTextarea.value);
+  };
+};
+
+if(window.attachEvent) {
+  window.attachEvent('onload', onLoad);
+} else {
+  if(window.onload) {
+    var existingOnLoad = window.onload;
+    var newOnLoad = function() {
+      existingOnLoad();
+      onLoad();
+    };
+    window.onload = newOnLoad;
+  } else {
+    window.onload = onLoad;
+  }
+}
