625b524119e519ad0a590dbaeaf2c50a164a1418
Bartlomiej Niemtur <nbartlomiej@gmail.com>
2011-09-07 21:15:36 +0200

    rewritten notifier, introduced variadic function evaluate()

--- spec/NotifierSpec.js
+++ spec/NotifierSpec.js
@@ -1,30 +1,54 @@
 describe("Notifier", function(){
+  var counter1;
+  var counter2;
   var notifier;
   beforeEach(function(){
-    notifier = new Notifier();
+      counter1 = new Counter({counters: new Array()});
+      counter2 = new Counter({counters: new Array()});
+      notifier = new Notifier(counter1, counter2);
   });
   describe("constructor", function(){
     it('adds self to the notifiers array of each argument', function(){
-      var counter1 = new Counter({counters: new Array()});
-      var counter2 = new Counter({counters: new Array()});
       new Array(counter1, counter2).forEach(function(counter){
         spyOn(counter.notifiers, 'push');
       });
-      var notifier = new Notifier(counter1, counter2);
+      notifier = new Notifier(counter1, counter2);
       new Array(counter1, counter2).forEach(function(counter){
         expect(counter.notifiers.push).toHaveBeenCalledWith(notifier);
       });
     });
-    it("initializes with empty dataTree", function(){
-      expect(notifier.dataTree).toBeEmptyObject();
+    it('adds arguments to the counters array', function(){
+      notifier = new Notifier(counter1, counter2);
+      expect(notifier.counters).toContain(counter1);
+      expect(notifier.counters).toContain(counter2);
     });
   });
 
   describe("callback(from, value)", function(){
-    it("assigns the result into two-level dataTree path", function(){
-      var result = jasmine.createSpy();
-      notifier.callback('one.two', result);
-      expect(notifier.dataTree.one.two).toEqual(result);
+    it("stores the result in the temporaryResults array", function(){
+      var counter = new Counter({counters: new Array()});
+      var notifier = new Notifier(counter);
+      var value = jasmine.createSpy('value');
+      notifier.callback(counter, value);
+      expect(notifier.temporaryResults).toContain(value);
+    });
+    it("positions temporaryResults according to parameter ordering", function(){
+      var value2 = jasmine.createSpy('value2');
+      notifier.callback(counter2, value2);
+      expect(notifier.temporaryResults[1]).toEqual(value2);
+    });
+    it("doesn't call evaluate() when not all parameters are present", function(){
+      spyOn(notifier, 'evaluate');
+      notifier.callback(counter1, jasmine.createSpy('value1'));
+      expect(notifier.evaluate).not.toHaveBeenCalled();
+    });
+    it("calls evaluate() when all parameters are present", function(){
+      var value1 = jasmine.createSpy('value1');
+      var value2 = jasmine.createSpy('value2');
+      spyOn(notifier, 'evaluate');
+      notifier.callback(counter1, value1);
+      notifier.callback(counter2, value2);
+      expect(notifier.evaluate).toHaveBeenCalledWith(value1, value2);
     });
   });
 });
--- src/Notifier.js
+++ src/Notifier.js
@@ -1,18 +1,24 @@
 function Notifier(){
+  this.counters = new Array();
+  this.temporaryResults = new Array();
   var that = this;
   // Converting arguments to Array object as we'd like to use the
   // Array.forEach() .
   argumentsArray = Array.prototype.slice.apply(arguments);
   argumentsArray.forEach(function(counter){
     counter.notifiers.push(that);
+    that.counters.push(counter);
   });
   this.dataTree = {};
 };
 
 Notifier.prototype.callback = function(from, value){
-  var pathArray = from.split('.');
-  var scanner = pathArray[0];
-  var property = pathArray[1];
-  if (this.dataTree[scanner] == undefined) {this.dataTree[scanner] = {}; }
-  this.dataTree[scanner][property] = value;
-}
+  var order = this.counters.indexOf(from);
+  this.temporaryResults[order] = value;
+  if (this.temporaryResults.length == this.counters.length){
+    this.evaluate.apply(this, this.temporaryResults);
+  }
+};
+
+Notifier.prototype.evaluate = function(){
+};
