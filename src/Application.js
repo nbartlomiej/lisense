029cf0d3533f1355faa7afadcdc075a3d70dcd62
Bartlomiej Niemtur <nbartlomiej@gmail.com>
2011-09-09 19:26:47 +0200

    automated readability index notifier (ariNotifier)

--- src/Application.js
+++ src/Application.js
@@ -5,9 +5,23 @@ var wordCounter = new Counter(wordScanner);
 
 var wordNotifier = new Notifier(wordCounter);
 wordNotifier.evaluate = function(wordCount){
-  if (wordCount > 10){
+  if (wordCount > 180){
+    console.log('Too many words per text: ' + wordCount);
     scannerGroup.notifications.push('something wrong');
   }
+};
+
+var characterScanner = new Scanner(scannerGroup, /\w/g);
+var characterCounter = new Counter(characterScanner);
+
+var sentenceScanner = new Scanner(scannerGroup, /[^\.]\./g);
+var sentenceCounter = new Counter(sentenceScanner);
+
+var ariNotifier = new Notifier(characterCounter, wordCounter, sentenceCounter);
+ariNotifier.evaluate = function(characterCount, wordCount, sentenceCount){
+  // var ariScore = 4.71 * (characterCount / wordCount) + 0.5 * (wordCount / sentenceCount) - 21.43;
+  var ariScore = (4.71 * (characterCount / wordCount)) + (0.5 * (wordCount / sentenceCount)) - 21.43;
+  console.log('Your ari score is: ' + ariScore);
 };
 
 
